5675343c8afc6fbef12a9d130b09df00fc7f8dcf
amandahnia80-rgb <amandahnia80@gmail.com>
2026-02-14 14:32:43 +0530

    Add hand tracking functionality to update player position

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 
+hands.onResults(results => {
+  if (results.multiHandLandmarks && results.multiHandLandmarks.length > 0) {
+    const hand = results.multiHandLandmarks[0];
+    const indexFinger = hand[8];
+
+    player.x = indexFinger.x * canvas.width;
+  }
+});
